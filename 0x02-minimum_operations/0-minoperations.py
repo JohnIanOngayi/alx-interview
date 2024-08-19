ffe43d6ed768b5da5420195dc3761c6dd56677d5
JohnIanOngayi <ongayijohnian@gmail.com>
2024-08-19 10:12:24 +0300

    chore: added function that returns min no. of operations required to attain a certain no. of characters
    Only commands available are: 'Copy All' and 'Paste'

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+In a text file, there is a single character H.
+Your text editor can execute only two operations in this file: Copy All n Paste
+Given a number n, write a method that calculates the fewest number of
+operations needed to result in exactly n H characters in the file.
+"""
+
+from typing import List
+
+
+def minOperations(n: float) -> int:
+    """
+    Returns min no. of operations to achieve n 'H' characters in a text file.
+
+    Parameters:
+    n (float): The target number of 'H' characters.
+
+    Returns:
+    int: The minimum number of operations.
+    """
+    return sum(prime_factors(n))
+
+
+def prime_factors(n: float) -> List[int]:
+    """
+    Compute the prime factors of a given number.
+
+    Parameters:
+    n (float): The number to factorize.
+
+    Returns:
+    List[int]: A list of prime factors of the number.
+    """
+    i: int = 2
+    factors: List[int] = []
+    while (n != 1):
+        if (n % i) == 0:
+            n = n / i
+            factors.append(i)
+        else:
+            i += 1
+    return factors
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,5 @@
+# 0x02-minimum_operations
+
+This repo folder contains fuction minOperations that calculates the fewest
+number of operations neeeded to result exactly n H characters in a file given
+the only commands are Copy All and Paste
