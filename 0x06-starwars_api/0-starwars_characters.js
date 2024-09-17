4e59278005494aff16fad8c4a3904ca7bfe3868a
JohnIanOngayi <ongayijohnian@gmail.com>
2024-09-17 06:51:33 +0300

    task: added swapi solution

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+if (process.argv.length < 3) {
+  console.log("Usage: ./0-starwars_characters.js <movie_id>");
+  process.exit(1);
+}
+id = process.argv[2];
+url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, resp, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+
+  const characterPromises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (err, resp, body) => {
+        if (err) reject(err);
+        resolve(JSON.parse(body).name);
+      });
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then((names) => {
+      names.forEach((name) => console.log(name));
+    })
+    .catch((err) => console.log(err));
+});
